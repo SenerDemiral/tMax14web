e6cbb708a88f6b966841edbdf3af46efe640d9de
SenerDemiral <sener.demiral@gmail.com>
2017-07-03 15:26:36 +0300

    170703

--- FB2SCservice/Scheduler.cs
+++ FB2SCservice/Scheduler.cs
@@ -22,21 +22,28 @@ namespace FB2SCservice
 
         protected override void OnStart(string[] args)
         {
+            FbLibrary.SendWithWebSocket.FrtSend("F");
+            FbLibrary.SendWithWebSocket.OpmSend("F");
+            FbLibrary.SendWithWebSocket.OphSend("F");
+
             timer1 = new Timer();
             timer1.Interval = 30000;    // 30sec
             timer1.Elapsed += Timer1_Elapsed;
             timer1.Enabled = true;
             FbLibrary.Logs.WriteErrorLog("FB2SC Service started");
+        
         }
 
         private void Timer1_Elapsed(object sender, ElapsedEventArgs e)
         {
-            //Library.WriteErrorLog("Timer ticked");
+            FbLibrary.Logs.WriteErrorLog("Timer ticked");
             //Library.FrtCron("F");
             //FbLibrary.Logs.WriteErrorLog("Timer ticked");
+            /*
             FbLibrary.SendWithWebSocket.FrtSend("F");
             FbLibrary.SendWithWebSocket.OpmSend("F");
             FbLibrary.SendWithWebSocket.OphSend("F");
+            */
         }
 
         protected override void OnStop()
