feddd370cc8db75f1de44c91bb8aae9422f5274e
SenerDemiral <sener.demiral@gmail.com>
2017-08-28 23:50:05 +0300

    170828

--- tMax14web/MapPage.json.cs
+++ tMax14web/MapPage.json.cs
@@ -2,30 +2,160 @@ using Starcounter;
 
 namespace tMax14web
 {
-    partial class MapPage : Json
+    [MapPage_json]
+    public partial class MapPage : Json
     {
-        [MapPage_json]
         protected override void OnData()
         {
             base.OnData();
 
-            LatLons.Clear();
+            MapList.Clear();
+            Markers.Clear();
 
-            LatLons.Add(new LatLonsElementJson
+            double LAT = 37.026835, LON = 27.563807;
+
+            /*
+            for (int i = 0; i < 10; i++)
+            {
+
+                //var a = new MarkersElementJson();
+                var a = new MarkersItem();
+
+                a.Lat = string.Format("{0}", LAT + (double)i / 100.0);
+                a.Lon = string.Format("{0}", LON + (double)i / 100.0);
+
+                Markers.Add(a);
+            }
+            */
+            /*
+            Markers.Add(new MarkersItem()
             {
                 Lat = "37.026835",
                 Lon = "27.563807",
                 Title = "Ev",
                 Info = "Bodrum Ev"
             });
-            
-            LatLons.Add(new LatLonsElementJson
-            { 
+            */
+
+            MapList.Add(new MapListItem()
+            {
+                Idx = 0,
+                Lat = "37.026835",
+                Lon = "27.563807",
+                Title = "Sener",
+                Info = "Demiral Ev"
+            });
+
+            MapList.Add(new MapListItem()
+            {
+                Idx = 1,
                 Lat = "37.033568",
                 Lon = "27.423496",
                 Title = "Marina",
-                Info = "Bodrum Limani"
+                Info = "<strong>Bodrum<br>Limani</strong>"
             });
+
+            MapList.Add(new MapListItem()
+            {
+                Idx = 2,
+                Lat = "37.096046",
+                Lon = "27.270502",
+                Title = "Nil",
+                Info = "<strong>Pakyurek<br>Ev</strong>"
+            });
+            /*
+                        Markers.Add(new MarkersItem()
+                        {
+                            Lat = "37.026835",
+                            Lon = "27.563807",
+                            Title = "Ev",
+                            Info = "Bodrum Ev"
+                        });
+                        Markers.Add(new MarkersItem()
+                        {
+                            Lat = "37.033568",
+                            Lon = "27.423496",
+                            Title = "Marina",
+                            Info = "Bodrum Limani"
+                        });*/
         }
+
+        [MapPage_json.Markers]
+        public partial class MarkersItem : Json
+        {
+            protected override void OnData()
+            {
+                base.OnData();
+            }
+
+            void Handle(Input.ClickTrigger Action)
+            {
+                //Open = Open ? false : true;
+                IsVisible = IsVisible ? false : true;
+                /*
+                var parent = (MapPage)this.Parent.Parent;
+                parent.Markers.Add(new MarkersItem()
+                {
+                    Lat = "37.04",
+                    Lon = "27.423496",
+                    Title = "Marina",
+                    Info = "Bodrum Limani"
+                });*/
+
+            }
+
+        }
+
+        [MapPage_json.MapList]
+        public partial class MapListItem : Json
+        {
+            void Handle(Input.Show Action)
+            {
+                var parent = (MapPage)this.Parent.Parent;
+                if (Show == 0)
+                {
+                    parent.Markers.Add(new MarkersItem()
+                    {
+                        Idx = this.Idx,
+                        Lat = this.Lat,
+                        Lon = this.Lon,
+                        Title = this.Title,
+                        Info = this.Info
+                    });
+                }
+                else
+                {
+                    ShowInfo = ShowInfo ? false : true;
+                    //parent.Markers[0].ShowInfo = ShowInfo;
+                    var ms = parent.Markers;
+                    foreach(var m in ms)
+                    {
+                        if (m.Idx == Idx)
+                        {
+                            m.ShowInfo = ShowInfo;
+                            break;
+                        }
+
+                    }
+                }
+            }
+        }
+        
+        //[MapPage_json.LatLons]
+            //[MapPage_json.Markers]
+            /*
+            partial class MarkersPage
+            {
+                protected override void OnData()
+                {
+                    base.OnData();
+                }
+
+                void Handle(Input.ClickTrigger action)
+                {
+
+                }
+            }*/
+        }
+
     }
-}
--- tMax14web/Program.cs
+++ tMax14web/Program.cs
@@ -16,25 +16,26 @@ namespace tMax14web
 				    <meta name=""viewport"" content=""width=device-width, initial-scale=1, shrink-to-fit=yes"">
 					<title>{0}</title>
 					<script src=""/sys/webcomponentsjs/webcomponents.min.js""></script>
-					
+					<script src=""/sys/document-register-element/build/document-register-element.js""></script>
+
 					<link rel=""import"" href=""/sys/polymer/polymer.html"">
 					<link rel=""import"" href=""/sys/starcounter.html"">
 					<link rel=""import"" href=""/sys/starcounter-include/starcounter-include.html"">
 					<script src=""/sys/thenBy.js""></script>
-					<link rel=""import"" href=""/sys/starcounter-debug-aid/src/starcounter-debug-aid.html"">
 					
-					<!--<link rel=""import"" href=""/sys/bootstrap.html"">
-					<script src=""/sys/webcomponentsjs/webcomponents.min.js""></script>
+					<!--
+                    <link rel=""import"" href=""/sys/bootstrap.html"">
 					<link rel=""import"" href=""/sys/starcounter-debug-aid/src/starcounter-debug-aid.html"">
+					<script src=""/sys/webcomponentsjs/webcomponents.min.js""></script>
 					<link rel=""import"" href=""/sys/iron-icons/maps-icons.html"">
 					<link rel=""stylesheet"" href=""https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"" integrity=""sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ"" crossorigin=""anonymous"">
 					
 					<link rel=""stylesheet"" href=""https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css"" crossorigin=""anonymous"">
 					<link rel=""stylesheet"" href=""/sys/Stylesheet1.css"">
+					<link rel=""import"" href=""/sys/github-browser-app.html"">
 					-->
 					<link rel=""stylesheet"" href=""/sys/Stylesheet2.css"">
 
-					<link rel=""import"" href=""/sys/github-browser-app.html"">
 					
 				
 				</head>
@@ -42,9 +43,9 @@ namespace tMax14web
 					<template is=""dom-bind"" id=""puppet-root"">
 						<starcounter-include view-model=""{{{{model}}}}"" partial-id$=""{{{{model.Html}}}}""></starcounter-include>
 					</template>
-					<puppet-client ref=""puppet-root"" remote-url=""{1}""></puppet-client>
-					<starcounter-debug-aid></starcounter-debug-aid>
+					<palindrom-client ref=""puppet-root"" remote-url=""{1}""></palindrom-client>
 					<!--
+					<starcounter-debug-aid></starcounter-debug-aid>
 					<script src=""https://code.jquery.com/jquery-3.1.1.slim.min.js"" integrity=""sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n"" crossorigin=""anonymous""></script>
 					<script src=""https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js"" integrity=""sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb"" crossorigin=""anonymous""></script>
 					<script src=""https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"" integrity=""sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn"" crossorigin=""anonymous""></script>
@@ -75,9 +76,17 @@ namespace tMax14web
 
             Handle.GET("/tMax14web/Map", () =>
             {
+                /*
                 var page = new MapPage();
                 page.Data = null;
                 return page; // new MapPage();
+                */
+                var master = (MasterPage)Self.GET("/tMax14web");
+                master.CurrentPage = new MapPage();
+                master.CurrentPage.Data = null;
+
+                return master;
+
             });
 
             Handle.GET("/tMax14web/d2", () =>
