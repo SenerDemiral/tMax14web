f3e0913b1d2d2dba351fa5a994350b420979f490
SenerDemiral <sener.demiral@gmail.com>
2017-10-03 09:37:26 +0300

    191003

---
+++ ZZZ/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Starcounter;
+using TMDB;
+
+namespace ZZZ
+{
+    class Program
+    {
+        static void Main()
+        {
+            //var f57036 = Db.SQL<TMDB.FRT>("select f from FRT f where f.FrtID = ?", 57036).First;
+        }
+    }
+}
